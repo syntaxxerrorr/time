0e44d1f30f47c59136a2a75476b934aa0c70f49f
Syntax Error <fahad@talwandi.net>
2015-06-06 16:55:16 +0500

    Initial Commit

---
+++ time/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  time
+//
+//  Created by Fahad Mustafa on 06/06/2015.
+//  Copyright (c) 2015 Fahad Mustafa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
