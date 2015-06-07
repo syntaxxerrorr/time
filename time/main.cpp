9076833757bbd20a3851c0ade93d2d40caaaaf8c
Syntax Error <fahad@talwandi.net>
2015-06-07 12:56:12 +0500

    1st commit

--- time/main.cpp
+++ time/main.cpp
@@ -8,8 +8,93 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+class time
+{
+public:
+    time()
+    {
+        hr=0;
+        min=0;
+        sec=0;
+    }
+    
+    int hr,min,sec;
+    
+    void read()
+    {
+        std::cout<<"Hours=";
+        std::cin>>hr;
+        std::cout<<"\nMinutes=";
+        std::cin>>min;
+        std::cout<<"\nSeconds=";
+        std::cin>>sec;
+    }
+    time operator +(time t2)
+    {
+        time t3;
+        t3.sec=sec+t2.sec;
+        if(t3.sec>60)
+        {
+            t2.min+=1;
+            t3.sec-=60;
+        }
+        t3.min=min+t2.min;
+        if(t3.min>60)
+        {
+            t2.hr+=1;
+            t3.min-=60;
+        }
+        t3.hr=hr+t2.hr;
+        return t3;
+    }
+    void display()
+    {
+        if(hr>=24)
+        {
+            hr=hr%24;
+        }
+        if(hr<10)
+        {
+            std::cout<<"0"<<hr;
+        }
+        else
+        {
+            std::cout<<hr;
+        }
+        if(min<10)
+        {
+            std::cout<<":0"<<min;
+        }
+        else
+        {
+            
+        std::cout<<":"<<min;
+        }
+        
+        if(sec<10)
+        {
+            std::cout<<":0"<<sec;
+        }
+        else
+        {
+            std::cout<<":"<<sec;
+        }
+    }
+};
+int main()
+{  
+    
+    class time c1,c2,c3;
+    std::cout<<"\n\nEnter the First Time\n\n";
+    c1.read();  
+    std::cout<<"\n\nEnter the Second Time\n\n";
+    c2.read();  
+    c3=c1+c2;  
+    std::cout<<"\n\nFirst Time  \t\t";
+    c1.display();  
+    std::cout<<"\n\nSecond Time  \t\t";
+    c2.display();  
+    std::cout<<"\n\nAfter addition,the time is ";
+    c3.display();  
+    getchar();
+}  
